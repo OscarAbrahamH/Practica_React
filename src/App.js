eda7f87337789e278edcae6bccc16fca0693c1d5
OscarAbrahamH <43403654+OscarAbrahamH@users.noreply.github.com>
2020-10-26 13:49:57 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    
+    <div className="App">
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">PRUEBA REACT</h1>
+    <p class="lead">Oscar Abraham Huerta Madueño - 17210897</p>
+    
+  </div>
+  <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Practica #1 React
+</button>
+
+</div>
+
+    </div>
+
+    
+  );
+}
+
+export default App;
